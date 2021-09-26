277aa1716ae744ffd7b5052312d759daf2a47507
malay patil <34594619+malay21@users.noreply.github.com>
2021-09-26 21:33:29 +0530

    micro code
    added microcode in pacstrap command

--- base-install.sh
+++ base-install.sh
@@ -4,9 +4,18 @@
 #cleans tty window
 clear
 
+#checking cpu for microcode
+
+CPU=$(grep vendor_id /proc/cpuinfo)
+if [[ $CPU == *"AuthenticAMD"* ]]
+then
+    microcode=amd-ucode
+else
+    microcode=intel-ucode
+fi
 
 #base install
-pacstrap /mnt base base-devel linux linux-firmware git vim
+pacstrap /mnt base base-devel linux linux-firmware git vim $microcode
 
 #copy 2nd script to installation drive
 cp pakages-install.sh /mnt/pakages-install.sh
